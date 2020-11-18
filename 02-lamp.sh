b09210001b56cb71d148b7622d0376f6df683a41
IGOR JORDÃO <53325396+Igu-jordan@users.noreply.github.com>
2020-11-17 21:38:46 -0300

    Add files via upload

--- 02-lamp.sh
+++ 02-lamp.sh
@@ -6,3 +6,4 @@ echo -e "Atualizando as Listas do Apt"
 
 echo -e "Instalando o LAMP server no Ubuntu Server"
     #obs: utilizar a expressão regular ^ que casa com todas as opções
+    apt -y install lamp-server^ perl python
