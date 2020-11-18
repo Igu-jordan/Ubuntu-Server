51487774b32d1c506885a2fc9db0883d89fb6820
IGOR JORDÃO <53325396+Igu-jordan@users.noreply.github.com>
2020-11-17 21:35:02 -0300

    Add files via upload

---
+++ 02-lamp.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+clear
+
+echo -e "Atualizando as Listas do Apt"
+    apt update
+
+echo -e "Instalando o LAMP server no Ubuntu Server"
+    #obs: utilizar a expressão regular ^ que casa com todas as opções
